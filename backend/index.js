817279e65572f9528e2a50501ce6c016442f2ea2
amit <amitgupta542173@gmail.com>
2025-11-12 09:29:33 +0000

    initial backend setup

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());    
+
+app.get('/' , (req , res) => {
+    console.log('Hello from Backend!');
+    res.json('Hello from Backend!');
+})
+
+app.listen(port , () => {
+    console.log(`Server is running on port ${port}`);
+})
+
